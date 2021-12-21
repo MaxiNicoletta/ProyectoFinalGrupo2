fc02515907678f2eee745d5d467cdc008ebee00c
De la Parra <Matias.DelaParra@sabre.com>
2021-12-21 12:02:09 -0300

    Implemented service

---
+++ DesafioFinal/src/main/java/com/example/DesafioSprint/DTOs/BookingsOrReservationsDTO.java
@@ -0,0 +1,13 @@
+package com.example.DesafioSprint.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookingsOrReservationsDTO {
+    Long firstId;
+    Long secondId;
+}
---
+++ DesafioFinal/src/main/java/com/example/DesafioSprint/Entities/BookingsOrReservations.java
@@ -0,0 +1,13 @@
+package com.example.DesafioSprint.Entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookingsOrReservations {
+    private Long firstId;
+    private Long secondId;
+}

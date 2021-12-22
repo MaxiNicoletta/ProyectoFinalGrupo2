829c98ca3559a009be48063eee889322636d372b
Matias <MD201285@fi365.ort.edu.uy>
2021-12-21 21:36:02 -0300

    Refactoring

--- DesafioFinal/src/main/java/com/example/DesafioSprint/DTOs/TouristicPackageDTO.java
+++ DesafioFinal/src/main/java/com/example/DesafioSprint/DTOs/TouristicPackageDTO.java
@@ -19,19 +19,4 @@ public class TouristicPackageDTO {
     private int bookResId1;
     private int bookResId2;
 
-//    public TouristicPackageDTO(int packageNumber, String name, String creation_date, int clientId) {
-//        this.packageNumber = packageNumber;
-//        this.name = name;
-//        this.creation_date = creation_date;
-//        this.clientId = clientId;
-//        BookingsOrReservations bookingsOrReservations = new BookingsOrReservations();
-//        bookingsOrReservations.setFirstId(1);
-//        bookingsOrReservations.setSecondId(2);
-//    }
-
-
-    //    private BookingPackageDTO bookingPackage;
-//    private BookingFlightPackageDTO bookingFlightPackage;
-//    private FlightReservationPackageDTO flightReservationPackage;
-
 }
--- DesafioFinal/src/main/java/com/example/DesafioSprint/Services/ServiceTouristicPackage.java
+++ DesafioFinal/src/main/java/com/example/DesafioSprint/Services/ServiceTouristicPackage.java
@@ -14,13 +14,9 @@ import java.util.List;
 public class ServiceTouristicPackage implements IServiceTouristicPackage {
 
     private final IPackageRepository packageRepository;
-    private final IBookingRepository bookingRepository;
-    private final IFligthReservationRepository fligthReservationRepository;
 
-    public ServiceTouristicPackage(IPackageRepository packageRepository, IBookingRepository bookingRepository, IFligthReservationRepository fligthReservationRepository) {
+    public ServiceTouristicPackage(IPackageRepository packageRepository) {
         this.packageRepository = packageRepository;
-        this.bookingRepository = bookingRepository;
-        this.fligthReservationRepository = fligthReservationRepository;
     }
 
     @Override
@@ -60,42 +56,3 @@ public class ServiceTouristicPackage implements IServiceTouristicPackage {
     }
 
 }
-
-//    private TouristicPackage getPackage(Long firstId,Long secondId) {
-//        TouristicPackage touristicPackage = new TouristicPackage();
-//
-//        FlightReservationPackage flightReservationPackage;
-//        BookingPackage bookingPackage;
-//        BookingFlightPackage bookingFlightPackage;
-//        Booking booking = bookingRepository.getById(firstId);
-//        Booking secondBooking = bookingRepository.getById(firstId);
-//        ReservaVuelo reservation = fligthReservationRepository.getById(firstId);
-//        ReservaVuelo secondReservation = fligthReservationRepository.getById(secondId);
-//        if (booking == null && secondBooking == null) {
-//            flightReservationPackage = new FlightReservationPackage(reservation, secondReservation);
-//            touristicPackage.setFlightReservationPackage(flightReservationPackage);
-//        }
-//        if (reservation == null && secondReservation == null) {
-//            bookingPackage = new BookingPackage(booking, secondBooking);
-//            touristicPackage.setBookingPackage(bookingPackage);
-//        }
-//        if (booking == null && reservation != null) {
-//            bookingFlightPackage = new BookingFlightPackage(secondBooking, reservation);
-//            touristicPackage.setBookingFlightPackage(bookingFlightPackage);
-//        }
-//        if (secondBooking == null && reservation != null) {
-//            bookingFlightPackage = new BookingFlightPackage(booking, reservation);
-//            touristicPackage.setBookingFlightPackage(bookingFlightPackage);
-//        }
-//        if (booking != null && reservation == null) {
-//            bookingFlightPackage = new BookingFlightPackage(booking, secondReservation);
-//            touristicPackage.setBookingFlightPackage(bookingFlightPackage);
-//        }
-//        if (secondBooking != null && reservation == null) {
-//            bookingFlightPackage = new BookingFlightPackage(secondBooking, secondReservation);
-//            touristicPackage.setBookingFlightPackage(bookingFlightPackage);
-//        }
-//        return touristicPackage;
-//    }
-//
-//}
